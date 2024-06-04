37b332b1fff8d33084bf0b58388b3d8b71d733da
panxin-2000 <2291162503@qq.com>
2024-06-04 23:45:58 +0800

    复原了glm中的第一个变换，但是不是使用的很正规的方法

--- main.cpp
+++ main.cpp
@@ -1,109 +1,33 @@
-#include <cassert>
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-typedef struct list {
-    int value;
-    struct list *next;
-} List;
+#include <iostream>
+#include <Eigen/Dense>
 
-
-void insert(List **head, int value) {
-    List **indirect = head;
-    List *tem = (list *) malloc(sizeof(*tem));
-    tem->value = value;
-    tem->next = NULL;
-
-    while (*indirect != NULL) {
-        indirect = &((*indirect)->next);
-    }
-    *indirect = tem;
-
-}
-
-/*
- * 下面的两个函数解决了指针和指向指针的指针的区别
- * 准确的说它们的区别其实一个int值有没有被多指一次
- */
-void pp_print_value(List **ptr) {
-    while (*ptr != NULL) {
-        printf("%d ", (*ptr)->value);
-        ptr = &(*ptr)->next;
-    }
-    printf("\n");
-}
-
-void p_print_value(List *ptr) {
-    while (ptr != NULL) {
-        printf("%d ", ptr->value);
-        ptr = ptr->next;
-    }
-    printf("\n");
-}
-
-void remove_list_entry(List **head, List *entry) {
-    List **indirect = head;
-
-    while ((*indirect) != entry) {
-        indirect = &(*indirect)->next;
-    }
-
-    *indirect = entry->next;
-}
-
-/*
- * 反转队列
- */
-
-List *invert_list(List **head) {
-
-    List *prev = NULL;
-    List *curr = *head;
-    while (curr != NULL) {
-        List *nextTemp = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = nextTemp;
-    }
-    return prev;
-}
-
+using Eigen::MatrixXd;
 
 int main() {
-    int pi;
-    pi = 2;
-    assert((7<pi));
-    char a[16]={"abcdef"};
-    printf("%d %d %d\n",sizeof (a),sizeof (*a),sizeof (&a));
-    printf("%d %d %d\n",strlen (a),strlen (( char *)&a));
 
 
-    List *head = NULL;//默认值不是0，而是一个其他的值，所以第一次运行的时候出问题了。
-    insert(&head, 1);
-    insert(&head, 2);
-    insert(&head, 3);
-    insert(&head, 4);
-    insert(&head, 5);
-    insert(&head, 6);
+//    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+//    std::cout << glm::to_string(vec) << std::endl;
+//    glm::mat4 trans = glm::mat4(1.0f);
+//    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+//    vec = trans * vec;
+    //正常应该是这个样子，但是打印处理是不太对。
+//    2.000000 = | (1.000000, 0.000000, 0.000000, 1.000000) | * 1.000000
+//    1.000000 = | (0.000000, 1.000000, 0.000000, 1.000000) | * 0.000000
+//    0.000000 = | (0.000000, 0.000000, 1.000000, 0.000000) | * 0.000000
+//    1.000000 = | (0.000000, 0.000000, 0.000000, 1.000000) | * 1.000000
+    Eigen::Vector4d v(1, 0, 0,1);
+    std::cout << v << std::endl;
 
 
-    p_print_value(head);//传递指针时，把申请的指针符号的值(也是一个地址)给参数就可以，传参的过程可以参考第12行的代码
-    head= invert_list(&head);
-    pp_print_value(&head);//传递双重指针时，把申请的指针符号的地址给参数就可以
+    MatrixXd m(4, 4);
 
-    List *rm2 = head->next;
-    List *rm4 = head->next->next->next;
+    m(0, 0) = 1;
+    m(1, 1) = 1;
+    m(2, 2) = 1;
+    m(1, 3) = 1;
+    m(0, 3) = 1;
 
-    remove_list_entry(&head, head);
-    p_print_value(head);
-
-    remove_list_entry(&head, rm2);
-    p_print_value(head);
-
-    remove_list_entry(&head, rm4);
-    p_print_value(head);
-
-
-    printf("hello");//不会在执行完这句后就立刻打印出来。
-    return 0;
+    std::cout << m << std::endl;
+    std::cout << m * v << std::endl;
 }
