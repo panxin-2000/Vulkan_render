2f43d1b5f6b1f7635e2d3199b724c6119698e8ac
panxin-2000 <2291162503@qq.com>
2024-11-11 22:42:14 +0800

    我也简单的用vector创建了两个线程

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
-
 #include <thread>
+#include <chrono>
 
-void increment(int &x) {
-    ++x;
+void print_message(const std::string &message, int delay) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+    std::cout << message << std::endl;
 }
 
 int main() {
-    int num = 0;
-    std::thread t1(increment, std::ref(num));
-    t1.join();
-    std::cout << "value after increment " << num << std::endl;
+    std::vector<std::thread> tem_threads;
+    tem_threads.push_back(std::thread(print_message, "hello 1", 1000));
+    tem_threads.push_back(std::thread(print_message, "hello 2", 500));
+    for (auto &tem_thread: tem_threads) {
+        if (tem_thread.joinable())
+            tem_thread.join();
+    }
+    std::cout << "main thread finish. " << std::endl;
     return 0;
 
 }
