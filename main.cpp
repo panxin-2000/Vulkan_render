bdaba3181679f31435166606ae01141e2142d022
panxin-2000 <2291162503@qq.com>
2024-11-11 21:27:34 +0800

    向量和线程的组合，很巧妙

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
-
+#include <vector>
 #include <thread>
+#include <string>
 
-void printMessage(int count) {
-    for (int i = 0; i < count; ++i) {
-        std::cout << "hello from thread (function pointer)" << std::endl;
-    }
+int count = 0;
+
+void doSomeWork() {
+    std::cout << "the doSomeWork function is running on another thread" << std::endl;
+    int data = count++;
+
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    std::string str = std::to_string(data);
+    std::cout << "the function call by the worker thread has ended. " + str << std::endl;
 }
 
+
 int main() {
-    std::thread t1(printMessage, 5);
-    t1.join();
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 10; ++i) {
+        threads.push_back(std::thread(doSomeWork));
+        std::cout << "the main thread calls this after starting the new tread" << std::endl;
+    }
+    for (auto &thread: threads) {
+        thread.join();
+    }
     return 0;
 
 }
