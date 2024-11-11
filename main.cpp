af22586e3e46528f4b9d382c8804f49416609596
panxin-2000 <2291162503@qq.com>
2024-11-11 21:52:02 +0800

    使用std::ref 传递引用，有其他的传递方式吗？

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
-#include <vector>
+
 #include <thread>
-#include <string>
 
-int count = 0;
-
-void doSomeWork() {
-    std::cout << "the doSomeWork function is running on another thread" << std::endl;
-    int data = count++;
-
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-    std::string str = std::to_string(data);
-    std::cout << "the function call by the worker thread has ended. " + str << std::endl;
+void increment(int &x) {
+    ++x;
 }
 
-
 int main() {
-    std::vector<std::thread> threads;
-    for (int i = 0; i < 10; ++i) {
-        threads.push_back(std::thread(doSomeWork));
-        std::cout << "the main thread calls this after starting the new tread" << std::endl;
-    }
-    for (auto &thread: threads) {
-        thread.join();
-    }
+    int num = 0;
+    std::thread t1(increment, std::ref(num));
+    t1.join();
+    std::cout << "value after increment " << num << std::endl;
     return 0;
 
 }
