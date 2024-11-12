ed457252f877921dadc692eb9f0acdca9c9a70cc
panxin-2000 <2291162503@qq.com>
2024-11-12 10:46:58 +0800

    更换可以手动解锁的？然后问题之前的一版，其实并没有保存，难道不能手动解锁吗？

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <mutex>
+
+std::mutex mtx;
+int shared_resource = 0;
 
 void print_message(const std::string &message, int delay) {
+    std::unique_lock<std::mutex> lock(mtx);
+    ++shared_resource;
+    lock.unlock(); //这里其实是稍微法线来一点问题的，那就是如果你不存储临时计算的结果，之后结果是会变的。
     std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-    std::cout << message << std::endl;
+    std::cout << message << shared_resource << std::endl;
 }
 
 int main() {
     std::vector<std::thread> tem_threads;
-    tem_threads.push_back(std::thread(print_message, "hello 1", 1000));
-    tem_threads.push_back(std::thread(print_message, "hello 2", 500));
+    tem_threads.push_back(std::thread(print_message, "hello 1 ", 500));
+    tem_threads.push_back(std::thread(print_message, "hello 2 ", 1000));
     for (auto &tem_thread: tem_threads) {
         if (tem_thread.joinable())
             tem_thread.join();
