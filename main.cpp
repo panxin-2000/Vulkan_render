0ae59285519e56812b7dd80d8937b7f128fdd984
panxin-2000 <2291162503@qq.com>
2025-01-16 15:13:04 +0800

    remove的一个函数的尝试

--- main.cpp
+++ main.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
-#include <mutex>
-#include <string>
-#include <thread>
-#include <filesystem>
-#include <random>
-#include "run_time.h"
+#include <list>
+#include <algorithm>
 
-struct Test {
-    uint32_t length;
-    std::vector<std::string> tem;  //vector 占据了6个字节
-    std::vector<uint32_t> le;
-} test;
+void Show(int);
 
-class run {
-private:
-    int32_t tm;
-    int32_t tmd;
-public:
-    uint32_t ren;
-    uint32_t rden;
-
-    run() {
-        std::cout << "init " << std::endl;
-    }
-};
-
+const int LIMIT = 10;
 
 int main() {
-    std::cout << "size of test " << sizeof(test) << std::endl;
-//    test.tem.push_back("sdsdsds");
-    std::cout << "size of test " << sizeof(test) << std::endl;
-//    test.le.push_back(35);
-    std::cout << "size of test " << sizeof(test) << std::endl;
-    std::vector<uint32_t> lem;
-    std::cout << "size of lem " << sizeof(lem) << std::endl;
-    lem.push_back(24);
-    lem.push_back(24);
-    lem.push_back(24);
-    std::cout << "size of lem " << sizeof(lem) << lem[1] << std::endl;
-    // lem[1] 这里开起来是c的操作，但是实际上是另一个种操作，因为向量的长度是固定的，不管添加的参数是什么。
-    class run rd;
-    std::cout << "size of rd " << sizeof(rd) << std::endl;
-
-
+    using namespace std;
+    int ar[LIMIT] = {4, 5, 4, 2, 2, 3, 4, 8, 1, 4};
+    list<int> la(ar, ar + LIMIT);
+    list<int> lb(la);
+    cout << "Original list contents: \n\t";
+    for_each(la.begin(), la.end(), Show);
+    cout << endl;
+    la.remove(4);
+    cout << "After using the remove() mothod:\n";
+    cout << "la:\t";
+    for_each(la.begin(), la.end(), Show);
+    cout << endl;
+    list<int>::iterator last;
+    last = remove(lb.begin(), lb.end(), 4);
+    cout << "after using the remove() function:\n";
+    cout << "lb: \t";
+    for_each(lb.begin(), lb.end(), Show);
+    cout << endl;
+    lb.erase(last, lb.end());
+    cout << " After using the erase() mothod: \n";
+    cout << "lb: \t";
+    for_each(lb.begin(), lb.end(), Show);
+    cout << endl;
     return 0;
+}
+
+void Show(int v) {
+    std::cout << v << ' ';
 }
