476e250d5d880e9561bcda00345853111890382f
panxin-2000 <2291162503@qq.com>
2025-01-17 20:12:41 +0800

    map和set不是很理解，也就是相关性容器不是很理解

--- main.cpp
+++ main.cpp
@@ -1,29 +1,59 @@
 #include <iostream>
-#include <list>
-#include <algorithm>
+#include <string>
 #include <vector>
+#include <set>
+#include <map>
+#include <iterator>
+#include <algorithm>
+#include <cctype>
 
-void Show(int);
+using namespace std;
 
-const int LIMIT = 10;
+char toLower(char ch) { return tolower(ch); }
+
+string &ToLower(string &st);
+
+void display(const string &s);
 
 int main() {
-    using namespace std;
+    vector<string> words;
+    cout << "Enter words (enter quit to quit) : \n";
+    string input;
+    while (cin >> input && input != "quit")
+        words.push_back(input);
+    cout << "You entered the following words:\n";
+    for_each(words.begin(), words.end(), display);
+    cout << endl;
 
-    string letters;
-    cout << "Enter the letter grouping (quit to quit):";
-    while (cin >> letters && letters != "quit") {
-        cout << "Permutations of " << letters << endl;
-        sort(letters.begin(), letters.end());
-        cout << letters << endl;
-        while (next_permutation(letters.begin(), letters.end())) {
-            cout << letters << endl;
-        }
-        cout << "Enter next sequence (quit to quit) :";
-    }
-    cout << "Done .\\n";
+    // place words in set, converting to lowercase
+    set<string> word_set;
+    transform(words.begin(), words.end(),
+              insert_iterator<set<string>>(word_set, word_set.begin()),
+              ToLower);
+    cout << "\n Alphabetic list of words: \n";
+    for_each(word_set.begin(), word_set.end(), display);
+    cout << endl;
+
+    // place word and frequency in map
+    map<string, int> word_map;
+    for (auto si = word_set.begin(); si != word_set.end(); ++si)
+        word_map[*si] = count(words.begin(),words.end(),*si);
+
+    //display map contents
+    cout << "\nWord frequency :\n";
+    for (auto si = word_set.begin(); si != word_set.end(); ++si)
+        cout << *si << ": " << word_map[*si] << endl;
+
     return 0;
 }
-//我今天才发现字符串原来是能够这么搞的，
-//以前我一只以为的都是尽量以单词为一组，比如在一个句子中查找其中一个单词
-//现在发现，居然能够对其中的每个字符都进行排序。
+
+string & ToLower (string & st){
+    transform(st.begin(),st.end(),st.begin(), toLower);
+    return st;
+}
+
+void display(const string &s){
+    cout << s << " ";
+}
+
+// 简答的抄完了，但是不理解
