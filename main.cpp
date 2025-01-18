ab0621fffb8cab9ad48c9f98cbd238a5dd74fa7c
panxin-2000 <2291162503@qq.com>
2025-01-18 16:31:57 +0800

    关于set的部分的内容，主要的感觉是键和值相同是为什么还需要关联，在set中提供哪些特殊的算法相对于其他容器？

--- main.cpp
+++ main.cpp
@@ -9,51 +9,60 @@
 
 using namespace std;
 
-char toLower(char ch) { return tolower(ch); }
-
-string &ToLower(string &st);
-
-void display(const string &s);
-
 int main() {
-    vector<string> words;
-    cout << "Enter words (enter quit to quit) : \n";
-    string input;
-    while (cin >> input && input != "quit")
-        words.push_back(input);
-    cout << "You entered the following words:\n";
-    for_each(words.begin(), words.end(), display);
+    const int N = 6;
+    string s1[N] = {"buffoon", "thinkers", "for", "heavy", "can", "for"};
+    string s2[N] = {"metal", "any", "food", "elegant", "deliver", "for"};
+
+    set<string> A{s1, s1 + N};
+    set<string> B{s2, s2 + N};
+
+    ostream_iterator<string, char> out(cout, " ");
+    cout << "Set A : ";
+    copy(A.begin(), A.end(), out);
+    cout << endl;
+    cout << "Set B : ";
+    copy(B.begin(), B.end(), out);
     cout << endl;
 
-    // place words in set, converting to lowercase
-    set<string> word_set;
-    transform(words.begin(), words.end(),
-              insert_iterator<set<string>>(word_set, word_set.begin()),
-              ToLower);
-    cout << "\n Alphabetic list of words: \n";
-    for_each(word_set.begin(), word_set.end(), display);
+    cout << "Union of A and B:\n";
+    set_union(A.begin(), A.end(), B.begin(), B.end(), out);
     cout << endl;
 
-    // place word and frequency in map
-    map<string, int> word_map;
-    for (auto si = word_set.begin(); si != word_set.end(); ++si)
-        word_map[*si] = count(words.begin(),words.end(),*si);
+    cout << "Intersection of A and B: \n";
+    set_intersection(A.begin(), A.end(), B.begin(), B.end(), out);
+    cout << endl;
 
-    //display map contents
-    cout << "\nWord frequency :\n";
-    for (auto si = word_set.begin(); si != word_set.end(); ++si)
-        cout << *si << ": " << word_map[*si] << endl;
+    cout << "Difference of A and B :\n";
+    set_difference(A.begin(), A.end(), B.begin(), B.end(), out);
+    cout << endl;
 
+    cout << "Difference of B and A :\n";
+    set_difference(B.begin(), B.end(), A.begin(), A.end(), out);
+    cout << endl;
+
+    set<string> C;
+    cout << "Set c:\n";
+    set_union(A.begin(), A.end(), B.begin(), B.end(),
+              insert_iterator<set<string>>(C, C.begin()));
+    copy(C.begin(), C.end(), out);
+    cout << endl;
+
+    string s3{"grungy"};
+    C.insert(s3);
+    cout << "Set C after insertion:\n";
+    copy(C.begin(), C.end(), out);
+    cout << endl;
+
+    cout << "showing a range :\n";
+    copy(C.lower_bound("ghost"), C.upper_bound("spook"), out);
+    cout << endl;
+    pair
     return 0;
 }
-
-string & ToLower (string & st){
-    transform(st.begin(),st.end(),st.begin(), toLower);
-    return st;
-}
-
-void display(const string &s){
-    cout << s << " ";
-}
-
-// 简答的抄完了，但是不理解
+// 这里的主要的问题是使用的less<>
+// template<_Key, _Compare = less<_Key> >
+// 省略了第二个模版参数，只输入了第一个key,应该是可以理解为键的意思
+// 对于最简单的关联容器 set ，其 值和键相同，键是唯一的。
+// map 中值和键的类型不同，其实是可以相同的，只是一般情况下不同。 键唯一。
+// map 可以用来加速搜索吗？
