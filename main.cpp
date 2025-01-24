35d50ed490cec6dc24424a4ea81ff5d1c3f94d0b
panxin-2000 <2291162503@qq.com>
2025-01-24 14:47:54 +0800

    没有办法使用异或求靠近的二次幂

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ static inline int ImUpperPowerOfTwo(int v) {
     v++;
     return v;
 }
+
 static inline int ImLowerPowerOfTwo(int v) {
     v |= v >> 1;
     v |= v >> 2;
@@ -31,6 +32,7 @@ static inline int ImLowerPowerOfTwo(int v) {
     v |= v >> 16;
     v++;
     return v>>1;  // 异或运算符是不行的，因为要把最高位后面全部变成0 最高位左移一位然后得到一个新的数，然后不断或，最好再异或应该是可以的
+    //异或是不行的
 }
 
 int main(int argc, char **argv) {
@@ -42,7 +44,7 @@ int main(int argc, char **argv) {
     std::cout << "ImUpperPowerOfTwo( 5 ) = " << ImUpperPowerOfTwo(5) << std::endl;
     std::cout << "ImUpperPowerOfTwo( 129 ) = " << ImUpperPowerOfTwo(129) << std::endl;
     // 一个很特殊的例子，129能够充分的演示为什么是这个结果。
-     std::cout << "ImLowerPowerOfTwo( 0 ) = " << ImLowerPowerOfTwo(0) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 0 ) = " << ImLowerPowerOfTwo(0) << std::endl;
     std::cout << "ImLowerPowerOfTwo( 1 ) = " << ImLowerPowerOfTwo(1) << std::endl;
     std::cout << "ImLowerPowerOfTwo( 2 ) = " << ImLowerPowerOfTwo(2) << std::endl;
     std::cout << "ImLowerPowerOfTwo( 3 ) = " << ImLowerPowerOfTwo(3) << std::endl;
