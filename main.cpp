9cd70cf3a9e2175173ef32450b51954993d38a5f
panxin-2000 <2291162503@qq.com>
2025-01-24 14:41:39 +0800

    两个还算简单的算术方法，求某个数最靠近的上或下二次幂

--- main.cpp
+++ main.cpp
@@ -12,45 +12,45 @@ void exit_function(int sig) {
     endwin();// 刚刚删除的掉的一行没有任何影响
     exit(0);
 }
+
+static inline int ImUpperPowerOfTwo(int v) {
+    v--;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v++;
+    return v;
+}
+static inline int ImLowerPowerOfTwo(int v) {
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v++;
+    return v>>1;  // 异或运算符是不行的，因为要把最高位后面全部变成0 最高位左移一位然后得到一个新的数，然后不断或，最好再异或应该是可以的
+}
+
 int main(int argc, char **argv) {
-    signal(SIGINT, exit_function);
-    int key;
-    initscr();
-    crmode();
-    keypad(stdscr, TRUE);
-    noecho();
-    clear();
-    refresh();
-    key = getch();
-    while (key != ERR && key != 'q') {
-        clrtoeol();
-        static int x = 10;
-        static int y = 10;
-        if ((key >= 'A' && key <= 'Z')
-            || (key >= 'a' && key <= 'z')) {
-//            std::cout << "Key was " << (char) key << std::endl;
-        } else {
-            switch (key) {
-                case KEY_DOWN:
-                    ++x;
-                    break;
-                case KEY_UP:
-                    --x;
-                    break;
-                case KEY_RIGHT:
-                    ++y;
-                    break;
-                case KEY_LEFT:
-                    --y;
-                    break;
-            }
-        }
-        move(x, y);
-        printw("**");
-        refresh();
-        key = getch();
-    }
-    endwin();
-    echo();
-    exit(EXIT_SUCCESS);
+    std::cout << "ImUpperPowerOfTwo( 0 ) = " << ImUpperPowerOfTwo(0) << std::endl;
+    std::cout << "ImUpperPowerOfTwo( 1 ) = " << ImUpperPowerOfTwo(1) << std::endl;
+    std::cout << "ImUpperPowerOfTwo( 2 ) = " << ImUpperPowerOfTwo(2) << std::endl;
+    std::cout << "ImUpperPowerOfTwo( 3 ) = " << ImUpperPowerOfTwo(3) << std::endl;
+    std::cout << "ImUpperPowerOfTwo( 4 ) = " << ImUpperPowerOfTwo(4) << std::endl;
+    std::cout << "ImUpperPowerOfTwo( 5 ) = " << ImUpperPowerOfTwo(5) << std::endl;
+    std::cout << "ImUpperPowerOfTwo( 129 ) = " << ImUpperPowerOfTwo(129) << std::endl;
+    // 一个很特殊的例子，129能够充分的演示为什么是这个结果。
+     std::cout << "ImLowerPowerOfTwo( 0 ) = " << ImLowerPowerOfTwo(0) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 1 ) = " << ImLowerPowerOfTwo(1) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 2 ) = " << ImLowerPowerOfTwo(2) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 3 ) = " << ImLowerPowerOfTwo(3) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 4 ) = " << ImLowerPowerOfTwo(4) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 5 ) = " << ImLowerPowerOfTwo(5) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 8 ) = " << ImLowerPowerOfTwo(8) << std::endl;
+    std::cout << "ImLowerPowerOfTwo( 129 ) = " << ImLowerPowerOfTwo(129) << std::endl;
+    // 一个很特殊的例子，129能够充分的演示为什么是这个结果。
+
+
 }
