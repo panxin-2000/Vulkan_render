a4bb2806f782de20eb684c0add82135f4804187b
panxin-2000 <2291162503@qq.com>
2025-09-17 23:42:34 +0800

    简单的完成两个数字相加，使用加法完成

--- add_char_calculata.cpp
+++ add_char_calculata.cpp
@@ -63,7 +63,64 @@ struct tree_node_calculate {
 
     int (*operate_function)(int left_value, int right_value);
 
-    int tree_add_after_node(struct tree_node_calculate *new_node) {
+    struct tree_node_calculate *find_miximum_leaf(struct tree_node_calculate *root) {
+        if (root == nullptr) {
+            return nullptr;
+        } else {
+            while (root->right != nullptr || root->left != nullptr) {
+                if (root->right != nullptr) {
+                    return find_miximum_leaf(root->right);
+                } else {
+                    return find_miximum_leaf(root->left);
+                }
+            }
+            return root;
+        }
+    }
+
+    struct tree_node_calculate *find_root(struct tree_node_calculate *root) {
+        if (root == nullptr) {
+            return nullptr;
+        } else {
+            while (root->parent != nullptr) {
+                return find_root(root->parent);
+            }
+            return root;
+        }
+    }
+
+    int tree_add_after_node(struct tree_node_calculate **root, struct tree_node_calculate *new_node) {
+        if (*root == nullptr) {
+            *root = new_node;
+        } else {
+            struct tree_node_calculate *new_root = *root;
+            struct tree_node_calculate *miximum_leaf = new_node->find_miximum_leaf(new_root);
+            if (miximum_leaf == new_root->find_root(new_root)) {
+                add_new_node_before_parent(miximum_leaf, new_node);
+            } else if (miximum_leaf->parent->right == nullptr) {
+                add_new_node_to_right(miximum_leaf->parent, new_node);
+            }
+        }
+    }
+
+    int add_new_node_to_right(struct tree_node_calculate *node, struct tree_node_calculate *new_node) {
+        new_node->right = node->right; // 新节点的右孩子更新为将要原本的右孩子
+        node->right = new_node; // 更新node的右孩子
+        new_node->parent = node; // 更新新节点的parent
+        if (new_node->right != nullptr) {
+            new_node->right->parent = new_node; // 如果原本的右孩子不为空，更新原本的父节点
+        }
+    }
+
+    int add_new_node_before_parent(struct tree_node_calculate *node, struct tree_node_calculate *new_node) {
+        new_node->parent = node->parent;
+        new_node->left = node;
+        node->parent = new_node;
+        if (new_node->parent != nullptr && new_node->parent->left == node) {
+            new_node->parent->left = new_node;
+        } else if (new_node->parent != nullptr && new_node->parent->right == node) {
+            new_node->parent->right = new_node;
+        }
     }
 
     int add_node_to_left_child(struct tree_node_calculate *left) {
@@ -173,34 +230,83 @@ TEST(calculate, BasicAssertions) {
  */
 // 过程应该是这样一个过程，写下来之后就会清楚很多
 // 那么就是明天中午，看看能不能搞定, // 已经中午了，发现上面的操作似乎都是在增加后继
+// 如果数字增加的计算符号的话，是增它的后继，不过这个后继是放在了父结点
+// 然后在增加时，判断最大的是否存在左孩子但是没有右孩子，如果是这中情况，就增加到右孩子
+// 再增加数字时，判断最大的有没有左右孩子，有的话就是左或右
+// 再增加符号时找到最大的数字(应该可以可以说是最大的叶子结点)，如果是左，将符号设置为其父，
+// 如果是右孩子，那就依次寻找其父亲，直到其是其父亲的左孩子或者到达根结点，
+
+// 上面的考虑的都是双目操作符，并没有考虑单目操作符，单目操作符的需要直接置要操作的数字为右孩子
+// 看起是能够成功的，
+// 如果是符号的话，找到最大的叶子结点，然后判断它是否根结点，是的话更新根结点；是左还是右？只能是右，
 
 // 第二天中午的时候，开始想了，首先发现了一个问题，那就是怎么分割符号，这个事情忘记应该怎么做了
 // 如果不考虑分割的话，那么我应该用什么形式来管理输入呢？
 // 不考虑分割，那么输入的内容就是 int argc, char **argv
+// 2 * (5 + 5) + (5 + 5 + 6)
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// &                   +           &                   +             &                   +
+// &         *              (      &         *              (        &         *              (
+// &    2        +           +     &    2        +                +  &    2        +                +
+// &           5   5       5   5   &           5   5         +       &           5   5         +        6
+//                                 &                       5   5     &                       5   5   
+//
+
+// 2 + 7 * (5 + 5) + (5 + 5 + 6)   左边是不行的，按照之前的说法是不对的，向上更新的时候还需要判断优先级
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+// &        +       &       +            &       +            &         +              &         +
+// &     2     7    &   2        *       &   2        *       &     2        *         &     2        *
+// &                &         7     (    &         7        ( &           7        (   &           7        (
+// &                &                    &                    &                  5     &                    +
+// &                &                    &                    &                        &                  5   5
+
 
 struct tree_node_calculate *construction_calulate_tree(int argc, char **argv) {
     if (argc == 0) {
         return nullptr;
     } else {
-        struct tree_node_calculate *root;
-        for (int i = 1; i < argc; i++) {
-            int string_length = strlen(argv[i]);
+        struct tree_node_calculate *root = nullptr;
+        for (int i = 0; i < argc; i++) {
+            int string_length = strlen(argv[i]); // 这行为什么一直有问题？// 原来是前面的for的开始位置有问题
             if (string_length == 1 && isascii(argv[i][0])) {
                 // 这里判断是字符的情况
-                for (int i = 0; i < sizeof(operate_symbol_and_functions) / sizeof(operate_symbol_and_function); i++) {
-                    if (operate_symbol_and_functions[i].symbol == argv[i][0]) {
-                        struct tree_node_calculate *new_node = init_a_calculate_note(
-                            0, operate_symbol_and_functions[i].symbol);
-                        root->tree_add_after_node(new_node);
-
+                // 下面一行的循环走不出来。
+                for (int j = 0; j < sizeof(operate_symbol_and_functions) / sizeof(operate_symbol_and_function); j++) {
+                    if (operate_symbol_and_functions[j].symbol == argv[i][0]) {
+                        struct tree_node_calculate *new_node =
+                                init_a_calculate_note(0, operate_symbol_and_functions[j].symbol);
+                        root->tree_add_after_node(&root, new_node);
+                        break;
                     }
                 }
             } else {
                 // 这里再判断是否是数字的情况
-
-
+                struct tree_node_calculate *new_node =
+                        init_a_calculate_note(atoi(argv[i]), 0);
+                root->tree_add_after_node(&root, new_node);
                 // 最后如果是非法字符，需要报错，等待更新后重新判断
             }
         }
+        return root;
     }
+}
+
+TEST(tree, tree_root) {
+    struct tree_node_calculate *new_node =
+            init_a_calculate_note(30, 0);
+    struct tree_node_calculate *new_node_2 =
+            init_a_calculate_note(0, '+');
+    new_node->add_new_node_before_parent(new_node, new_node_2);
+    EXPECT_EQ(new_node_2, new_node->find_root(new_node));
+}
+
+
+TEST(calculate, string_array_about_number) {
+    char *calculate_expreesion[3] = {
+        "30", "+", "50"
+    };
+    struct tree_node_calculate *tem = construction_calulate_tree(3, static_cast<char **>(calculate_expreesion));
+    struct tree_node_calculate *root = tem->find_root(tem);
+    calculate_subtree(root);
+    EXPECT_EQ(root->value, 80);
 }
