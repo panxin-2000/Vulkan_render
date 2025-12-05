ca241b33a1e19b7419a5036ea693a9d0dc281cd3
panxin-2000 <2291162503@qq.com>
2025-12-05 21:33:20 +0800

    线段与AABB相交，有思路了，线段，射线，直线的思路是一致的

--- include/base_element/intersect/objects_intersect_with_AABB.h
+++ include/base_element/intersect/objects_intersect_with_AABB.h
@@ -4,5 +4,32 @@
 
 #ifndef HELLO_MAC_OBJECTS_INTERSECT_WITH_AABB_H
 #define HELLO_MAC_OBJECTS_INTERSECT_WITH_AABB_H
+#include "base_element/geometry/AABB_bounding_box.h"
+
+template<typename T>
+bool intersect(const AABB_min_max<T> &L_box, const AABB_min_max<T> &R_box) {
+    // 这里尽量还是转换之后再去判断会更好一点
+    if (intersect(AABB_centroid<T>(L_box), AABB_centroid<T>(R_box))) {
+        return true;
+    }
+    return false;
+}
+
+
+/**
+ *  应该是需要分开坐标轴去判断
+ * @tparam T
+ * @param L_box
+ * @param R_box
+ * @return
+ */
+template<typename T>
+bool intersect(const AABB_centroid<T> &L_box, const AABB_centroid<T> &R_box) {
+    if ((L_box.centroid_point - R_box.centroid_point) <=
+        L_box.direction_interval + R_box.direction_interval)
+        return true;
+    return false;
+}
+
 
 #endif //HELLO_MAC_OBJECTS_INTERSECT_WITH_AABB_H
--- include/base_element/intersect/objects_intersect_with_segment.h
+++ include/base_element/intersect/objects_intersect_with_segment.h
@@ -8,8 +8,15 @@
 #include "../point_2.h"
 #include "../geometry/AABB_bounding_box.h"
 #include "objects_intersect_with_point.h"
+#include "base_element/intersect/objects_intersect_with_AABB.h"
 
-inline bool intersect(Segment<Point_2> &L_segment, Segment<Point_2> &R_segment) {
+inline bool intersect(const Segment<Point_2> &L_segment, const Segment<Point_2> &R_segment) {
+    AABB_min_max<Point_2> L_AABB{L_segment.start_point, L_segment.end_point};
+    AABB_min_max<Point_2> R_AABB{R_segment.start_point, R_segment.end_point};
+    if (!intersect(L_AABB, R_AABB)) {
+        return false;
+    }
+
     Point_2 ab = L_segment.end_point - L_segment.start_point;
     Point_2 ac = R_segment.start_point - L_segment.start_point;
     Point_2 ad = R_segment.end_point - L_segment.start_point;
@@ -45,4 +52,56 @@ inline bool intersect(Segment<Point_2> &L_segment, Segment<Point_2> &R_segment) 
     return false;
 }
 
+
+template<typename T>
+bool intersect(const AABB_centroid<T> &L_box, const Segment<T> &R_segment) {
+    AABB_min_max<Point_2> R_AABB{R_segment.start_point, R_segment.end_point};
+    if (!intersect(L_box, R_AABB)) {
+        return false;
+    }
+    return true;
+
+    return false;
+}
+
+template<typename T>
+bool intersect(const AABB_min_max<T> &L_box, const Segment<T> &R_segment) {
+    /**
+     *     找到四个 # 号的点，判断是否点是否在包围盒上并且在线段内，在则相交
+     *     *
+     *        *
+     *           *
+     *              #
+     *                 *
+     *              * * * # * * * * * * * * *
+     *              *        *              *
+     *              *           *           *
+     *              *              *        *
+     *              *                 *     *
+     *              *                    *  *
+     *              *                       #
+     *              *                       *  *
+     *              * * * * * * * * * * * * *     #
+     *                                               *
+     *                                                  *
+     */
+
+
+    // 存在相交
+    return true;
+
+    return false;
+
+    // 求最近点时，有一个稍微简单一点点办法，比较偏向于直线了
+    // 找到最近点两个 # 点
+    // 利用相似三角形可以比较快的得出结果
+}
+
+template<typename T>
+bool intersect_with_closest_result(const AABB_min_max<T> &L_box, const Segment<T> &R_segment, const T *result) {
+
+    // 求最近点时，有一个稍微简单一点点办法，比较偏向于直线了
+    // 找到最近点两个 # 点
+    // 利用相似三角形可以比较快的得出结果
+}
 #endif //HELLO_MAC_OBJECTS_INTERSECT_WITH_SEGMENT_H
