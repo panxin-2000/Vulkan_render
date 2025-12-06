64045cc20f5d95355b9793db2d770aa6ad9f403b
panxin-2000 <2291162503@qq.com>
2025-12-06 13:32:10 +0800

    暂时写的没有问题了，等吃饭过后回来测试

--- include/base_element/intersect/objects_intersect_with_segment.h
+++ include/base_element/intersect/objects_intersect_with_segment.h
@@ -64,24 +64,87 @@ bool intersect(const AABB_centroid<T> &L_box, const Segment<T> &R_segment) {
     return false;
 }
 
+
+template<typename Point_2>
+bool find_axis_aligned_four_point(const AABB_min_max<Point_2> &L_box,
+                                  const Segment<Point_2> &R_segment,
+                                  std::vector<Point_2> *result) {
+    auto offset = R_segment.end_point - R_segment.start_point;
+    float k_x = offset.y / offset.x;
+    float k_y = offset.x / offset.y;
+    float interval_to_min_x = L_box.min_point.x - R_segment.start_point.x;
+    float interval_to_max_x = L_box.max_point.x - R_segment.start_point.x;
+    float interval_to_min_y = L_box.min_point.y - R_segment.start_point.y;
+    float interval_to_max_y = L_box.max_point.y - R_segment.start_point.y;
+    float segment_min_x = std::min(R_segment.start_point.x, R_segment.end_point.x);
+    float segment_max_x = std::max(R_segment.start_point.x, R_segment.end_point.x);
+    float segment_min_y = std::min(R_segment.start_point.y, R_segment.end_point.y);
+    float segment_max_y = std::max(R_segment.start_point.y, R_segment.end_point.y);
+    // result_1 = {L_box.min_point.x, R_segment.start_point.y + k_x * start_point_to_min_x};
+    // result_2 = {L_box.max_point.x, R_segment.start_point.y + k_x * start_point_to_max_x};
+    // result_3 = std::min(R_segment.start_point.y, R_segment.end_point.y);
+    // result_4 = {R_segment.start_point.x + k_y * start_point_to_max_y, L_box.max_point.y};
+    // 判断有点多，不知道能不能省一点内容
+    auto number = 0;
+    auto a_y = R_segment.start_point.y + k_x * interval_to_min_x;
+    auto a_x = R_segment.start_point.x + interval_to_min_x;
+    if (a_y >= L_box.min_point.y && L_box.max_point.y >= a_y &&
+        a_x >= segment_min_x &&
+        segment_max_x <= a_x
+    ) {
+        result->push_back({L_box.min_point.x, a_y});
+        number++;
+    }
+    auto c_y = R_segment.start_point.y + k_x * interval_to_max_x;
+    auto c_x = R_segment.start_point.x + interval_to_max_x;
+    if (c_y >= L_box.min_point.y && L_box.max_point.y >= c_y &&
+        c_x >= segment_min_x &&
+        segment_max_x <= c_x) {
+        result->push_back({L_box.max_point.x, c_y});
+        number++;
+    }
+    auto b_x = R_segment.start_point.x + k_y * interval_to_min_y;
+    auto b_y = R_segment.start_point.y + k_y * interval_to_min_y;
+    if (b_x >= L_box.min_point.x && L_box.max_point.x >= b_x &&
+        b_y >= segment_min_y &&
+        segment_max_y <= b_y) {
+        result->push_back({b_x, L_box.min_point.y});
+        number++;
+    }
+    auto d_x = R_segment.start_point.x + k_y * interval_to_max_y;
+    auto d_y = R_segment.start_point.y + k_y * interval_to_max_y;
+    if (d_x >= L_box.min_point.x && L_box.max_point.x >= d_x &&
+        d_y >= segment_min_y &&
+        segment_max_y <= d_y) {
+        result->push_back({d_x, L_box.max_point.y});
+        number++;
+    }
+    if (number > 0)
+        return true;
+    return false;
+}
+
+// 之后还需要两个函数，返回的点是否在线段上，是否在光线上
+// 其实在上面也是能够判断完成的
+
 template<typename T>
-bool intersect(const AABB_min_max<T> &L_box, const Segment<T> &R_segment) {
+bool intersect(const AABB_min_max<Point_2> &L_box, const Segment<Point_2> &R_segment) {
     /**
      *     找到四个 # 号的点，判断是否点是否在包围盒上并且在线段内，在则相交
-     *     *
+     *     S
      *        *
      *           *
-     *              #
-     *                 *
-     *              * * * # * * * * * * * * *
+     *              #(a_y)
+     *                 *  (d_x)
+     *              d * * # * * * * * * * * c
      *              *        *              *
      *              *           *           *
      *              *              *        *
      *              *                 *     *
      *              *                    *  *
-     *              *                       #
+     *              *                       # (c_y)
      *              *                       *  *
-     *              * * * * * * * * * * * * *     #
+     *              a * * * * * * * * * * * b     # (b_x)
      *                                               *
      *                                                  *
      */
@@ -99,9 +162,17 @@ bool intersect(const AABB_min_max<T> &L_box, const Segment<T> &R_segment) {
 
 template<typename T>
 bool intersect_with_closest_result(const AABB_min_max<T> &L_box, const Segment<T> &R_segment, const T *result) {
-
     // 求最近点时，有一个稍微简单一点点办法，比较偏向于直线了
     // 找到最近点两个 # 点
     // 利用相似三角形可以比较快的得出结果
 }
+
+
+template<typename T>
+bool intersect(const Sphere<T> &sphere, const Segment<T> &R_segment) {
+    // 与球相交与判断结果之间是存在一个优化的办法的
+    // 在光线追踪的最简实现中看到过
+    // 优化了一元二次方程
+}
+
 #endif //HELLO_MAC_OBJECTS_INTERSECT_WITH_SEGMENT_H
