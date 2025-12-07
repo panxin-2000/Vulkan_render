2816b6c595a2bd5ed402c4365902abcc250fd52c
panxin-2000 <2291162503@qq.com>
2025-12-07 16:05:03 +0800

    球与三角形和球与球相交

--- include/base_element/intersect/objects_intersect_with_sphere.h
+++ include/base_element/intersect/objects_intersect_with_sphere.h
@@ -10,17 +10,40 @@
 #include "base_element/intersect/objects_intersect_with_segment.h"
 
 template<typename T>
-bool intersect(const Sphere<T> &sphere, const Triangle<T> &triangle) {
+bool intersect(const Sphere<T> &L_sphere, const Triangle<T> &triangle) {
     // 有更优的方案，写起来稍微麻烦一点
-    if (intersect(sphere, Segment<T>{triangle.a, triangle.b})) {
+    if (intersect(L_sphere, Segment<T>{triangle.a, triangle.b})) {
         return true;
     }
-    if (intersect(sphere, Segment<T>{triangle.b, triangle.c})) {
+    if (intersect(L_sphere, Segment<T>{triangle.b, triangle.c})) {
         return true;
     }
-    if (intersect(sphere, Segment<T>{triangle.c, triangle.a})) {
+    if (intersect(L_sphere, Segment<T>{triangle.c, triangle.a})) {
         return true;
     }
     return false;
 }
+
+/**
+ * 两个圆心的距离小于半径距离之和则相交
+ * @tparam T
+ * @param L_sphere
+ * @param R_sphere
+ * @return
+ */
+template<typename T>
+bool intersect(const Sphere<T> &L_sphere, const Sphere<T> &R_sphere) {
+    auto distance = L_sphere.center - R_sphere.center;
+    auto distanceSquared = dot(distance, distance);
+    if (distanceSquared <= L_sphere.radius * L_sphere.radius + R_sphere.radius * R_sphere.radius) {
+        return true;
+    }
+    return false;
+}
+
+// 获取相交的结果的时候，其实就不太好表示了
+// 两个圆相交之后的结果也是一个圆，或者一个点
+// 或者拿到的结果是相交之后体积， 怎么表示这个圆呢？
+
+
 #endif //HELLO_MAC_OBJECTS_INTERSECT_WITH_SPHERE_H
