d68777763943506283bae035e025eba156bb7fcf
panxin-2000 <2291162503@qq.com>
2026-01-14 17:38:04 +0800

    这种实例化的方式有问题

--- include/ECS.h
+++ include/ECS.h
@@ -7,9 +7,21 @@
 #include <entt/entt.hpp>
 
 
-static inline entt::registry &get_entt_instance() {
-    static auto *instance = new entt::registry;
-    return *instance;
+class World {
+public:
+    // 获取全局唯一的注册表引用
+    static entt::registry &get() {
+        static entt::registry instance;
+        return instance;
+    }
+
+private:
+    World() = default; // 禁用构造
+};
+
+
+static entt::registry &get_entt_instance() {
+    return World::get();
 }
 
 
