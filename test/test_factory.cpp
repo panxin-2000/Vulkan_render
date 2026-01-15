09c1b89f5fd15bf363e347a0dd342168be48697b
panxin-2000 <2291162503@qq.com>
2026-01-15 13:46:03 +0800

    工厂方法更改为UI相关的

--- test/test_factory.cpp
+++ test/test_factory.cpp
@@ -10,33 +10,46 @@
 #include "gtest/gtest.h"
 
 // 1. 产品基类
-class Document {
+class button {
 public:
-    virtual ~Document() = default;
+    virtual ~button() = default;
 
-    virtual void open() = 0;
+    virtual void draw() = 0;
 };
 
 // 2. 具体产品
-class PdfDocument : public Document {
+class mac_button : public button {
 public:
-    void open() override { std::cout << "打开 PDF..." << std::endl; }
+    mac_button() : button() {
+        std::cout << "创建 [macOS 风格] 柔和圆角按钮\n";
+    }
+
+    void draw() override {
+        std::cout << "绘制 [macOS 风格] 柔和圆角按钮\n";
+    }
 };
 
-class WordDocument : public Document {
+class win_button : public button {
 public:
-    void open() override { std::cout << "打开 Word..." << std::endl; }
+    win_button() : button() {
+        std::cout << "创建 [Windows 风格] 扁平化按钮\n";
+    }
+
+
+    void draw() override {
+        std::cout << "绘制 [Windows 风格] 柔和圆角按钮\n";
+    }
 };
 
 // 3. 统一工厂类（使用注册制）
-class DocumentFactory {
+class buttonFactory {
 public:
     // 定义一个函数类型，用于创建对象
-    using Creator = std::function<std::unique_ptr<Document>()>;
+    using Creator = std::function<std::unique_ptr<button>()>;
 
     // 核心：静态注册表
-    static DocumentFactory &instance() {
-        static DocumentFactory factory;
+    static buttonFactory &instance() {
+        static buttonFactory factory;
         return factory;
     }
 
@@ -46,7 +59,7 @@ public:
     }
 
     // 根据字符串创建对象
-    std::unique_ptr<Document> create(const std::string &name) {
+    std::unique_ptr<button> create(const std::string &name) {
         if (registry.find(name) != registry.end()) {
             return registry[name](); // 调用注册好的构造函数
         }
@@ -56,25 +69,25 @@ public:
 private:
     std::map<std::string, Creator> registry;
 
-    DocumentFactory() = default; // 单例模式
+    buttonFactory() = default; // 单例模式
 };
 
 // 4. 使用示例
 TEST(factory, factory_method) {
     // 在程序初始化时注册产品（这部分可以放在各自的实现文件中）
-    DocumentFactory::instance().registerType("pdf", []() {
-        return std::make_unique<PdfDocument>();
+    buttonFactory::instance().registerType("mac", []() {
+        return std::make_unique<mac_button>();
     });
-    DocumentFactory::instance().registerType("word", []() {
-        return std::make_unique<WordDocument>();
+    buttonFactory::instance().registerType("win", []() {
+        return std::make_unique<win_button>();
     });
 
     // 业务逻辑：只需传递字符串
-    std::string userInput = "pdf";
-    auto doc = DocumentFactory::instance().create(userInput);
+    std::string userInput = "mac";
+    auto button = buttonFactory::instance().create(userInput);
 
-    if (doc) {
-        doc->open();
+    if (button) {
+        button->draw();
     } else {
         std::cout << "不支持的文档格式" << std::endl;
     }
