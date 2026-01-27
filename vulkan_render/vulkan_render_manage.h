012e7b955346880acb884bd74c6448ca5829795d
panxin-2000 <2291162503@qq.com>
2026-01-27 23:23:49 +0800

    创建了一个新的 vk 管理类，主要想用它来管理资源

---
+++ vulkan_render/vulkan_render_manage.h
@@ -0,0 +1,116 @@
+//
+// Created by 潘鑫 on 2026/1/27.
+//
+
+#ifndef HELLO_MAC_VULKAN_RENDER_MANAGE_H
+#define HELLO_MAC_VULKAN_RENDER_MANAGE_H
+#include <map>
+#include <mutex>
+#include <thread>
+
+
+struct shader_and_share;
+class logic_render_data;
+
+class vk_render_manage {
+private:
+    mutable std::mutex mtx;
+    // std::vector<union_render_data> render_objects;
+    std::vector<logic_render_data *> need_init;
+    std::vector<logic_render_data *> need_update;
+    std::vector<logic_render_data *> need_clean;
+
+    std::atomic<bool> have_object_need_update = false;
+    std::atomic<bool> need_render = true;
+
+    std::map<std::string, shader_and_share> vertex_shader_map_;
+    std::map<std::string, shader_and_share> fragment_shader_map_;
+    std::map<std::string, shader_and_share> geometry_shader_map_;
+    // std::map<std::string, texture_and_share> texture_map_;
+    // std::map<Vertices_type, buffer_and_share> vertices_map_;
+    // std::map<Indices_type, buffer_and_share> indices_map_;
+
+public:
+    void init_logic_need_resources();
+
+    static vk_render_manage &get_instance() {
+        static vk_render_manage *instance = nullptr;
+        static std::once_flag flag;
+        std::call_once(flag, []() {
+            instance = new vk_render_manage();
+        });
+        return *instance;
+    }
+
+
+    void update_need_objects() {
+        for (auto user_render_component: need_update) {
+        }
+        need_update.clear();
+    }
+
+    void init_need_objects() {
+        need_clean.clear();
+    }
+
+    void clean_need_objects() {
+        need_clean.clear();
+    }
+
+
+    void add_render_object_need_init(logic_render_data *render_object) {
+        std::unique_lock<std::mutex> lock(mtx);
+        need_init.push_back(render_object);
+    }
+
+    void add_render_object_need_update(logic_render_data *render_object) {
+        std::unique_lock<std::mutex> lock(mtx);
+        need_update.push_back(render_object);
+    }
+
+    void add_render_object_need_clean(logic_render_data *render_object) {
+        std::unique_lock<std::mutex> lock(mtx);
+        need_clean.push_back(render_object);
+    }
+
+
+    void render_thread() {
+        while (need_render) {
+            {
+                std::unique_lock<std::mutex> lock(mtx);
+                init_need_objects(); // 主要是复制内存的操作
+                update_need_objects();
+            }
+            render_object_function();
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            clean_need_objects();
+        }
+        have_object_need_update = false;
+        need_render = true;
+    }
+
+    void render_thread_stop() {
+        std::unique_lock<std::mutex> lock(mtx);
+        need_render = false;
+        render_objects.clear();
+        need_init.clear();
+    }
+
+private:
+    vk_render_manage() {
+    }
+
+    ~vk_render_manage() {
+    }
+
+    vk_render_manage(const vk_render_manage &) = delete;
+
+    vk_render_manage &operator=(const vk_render_manage &) = delete;
+
+    vk_render_manage(vk_render_manage &&) = delete;
+
+    vk_render_manage &operator=(vk_render_manage &&) = delete;
+};
+
+
+#endif //HELLO_MAC_VULKAN_RENDER_MANAGE_H
