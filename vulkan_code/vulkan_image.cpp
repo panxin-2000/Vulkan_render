01531167698251163fdd78f0b19123429b8380f1
panxin-2000 <2291162503@qq.com>
2026-02-06 22:27:36 +0800

    只是更改了一个 sky cube 的一个简单的理论参数

--- vulkan_code/vulkan_image.cpp
+++ vulkan_code/vulkan_image.cpp
@@ -30,6 +30,27 @@ VkImageView createImageView(const VKDevice &handle,
         throw std::runtime_error("failed to create image view!");
     }
     return imageView;
+}VkImageView create_sky_cube_ImageView(const VKDevice &handle,
+                            const VkImage image,
+                            const VkFormat format,
+                            const VkImageAspectFlags aspectFlags,
+                            uint32_t mipLevels) {
+    VkImageViewCreateInfo viewInfo{};
+    viewInfo.sType                           = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    viewInfo.image                           = image;
+    viewInfo.viewType                        = VK_IMAGE_VIEW_TYPE_CUBE;
+    viewInfo.format                          = format;
+    viewInfo.subresourceRange.aspectMask     = VK_IMAGE_ASPECT_COLOR_BIT;
+    viewInfo.subresourceRange.baseMipLevel   = 0;
+    viewInfo.subresourceRange.levelCount     = 1;
+    viewInfo.subresourceRange.baseArrayLayer = 0;
+    viewInfo.subresourceRange.layerCount     = mipLevels;
+    viewInfo.subresourceRange.aspectMask     = aspectFlags;
+    VkImageView imageView;
+    if (vkCreateImageView(handle.get_device(), &viewInfo, nullptr, &imageView) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create image view!");
+    }
+    return imageView;
 }
 
 
@@ -43,7 +64,37 @@ uint32_t findMemoryType(VkPhysicalDevice physicalDevice, uint32_t typeFilter, Vk
     }
     throw std::runtime_error("failed to find suitable memory type!");
 }
+std::pair<VkImage, VmaAllocation> create_sky_cube_Image(VKDevice &handle, uint32_t width, uint32_t height, uint32_t mipLevels,
+                                              VkFormat format,
+                                              VkImageTiling tiling,
+                                              VkImageUsageFlags usage) {
+    VkImageCreateInfo imageInfo{};
+    imageInfo.sType         = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+    imageInfo.imageType     = VK_IMAGE_TYPE_2D;
+    imageInfo.extent.width  = width;
+    imageInfo.extent.height = height;
+    imageInfo.extent.depth  = 1;
+    imageInfo.mipLevels     = mipLevels;
+    imageInfo.arrayLayers   = 6;
+    imageInfo.format        = format;
+    imageInfo.tiling        = tiling;
+    imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+    imageInfo.usage         = usage;
+    imageInfo.samples       = VK_SAMPLE_COUNT_1_BIT;
+    imageInfo.sharingMode   = VK_SHARING_MODE_EXCLUSIVE;
 
+    VmaAllocationCreateInfo allocInfo = {};
+    allocInfo.usage                   = VMA_MEMORY_USAGE_AUTO; // 让 VMA 自动选最快的显存
+    // 对于 Image，通常不需要 HOST_ACCESS，因为我们走 Staging 流程
+    // 如果你强制要 CPU 可见，通常只能用 TILING_LINEAR，性能很差
+
+    VkImage image;
+    VmaAllocation allocation;
+    VmaAllocationInfo resultInfo;
+    vmaCreateImage(handle.get_allocator(), &imageInfo, &allocInfo, &image, &allocation, &resultInfo);
+
+    return {image, allocation};
+}
 
 std::pair<VkImage, VmaAllocation> createImage(VKDevice &handle, uint32_t width, uint32_t height, uint32_t mipLevels,
                                               VkFormat format,
@@ -344,7 +395,7 @@ VkSampler createTextureSampler(VKDevice &handle) {
     samplerInfo.sType        = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
     samplerInfo.magFilter    = VK_FILTER_LINEAR;
     samplerInfo.minFilter    = VK_FILTER_LINEAR;
-    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT; // sky_cube VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE
     samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
     samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
 
