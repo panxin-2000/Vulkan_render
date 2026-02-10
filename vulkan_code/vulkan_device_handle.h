ec2be6fdead78abd91f8244afb831bfafbceba16
panxin-2000 <2291162503@qq.com>
2026-02-10 16:58:23 +0800

    blender 中有具体的预分配 类型 与 数值,但是它是每次都创建一个 pool

--- vulkan_code/vulkan_device_handle.h
+++ vulkan_code/vulkan_device_handle.h
@@ -157,21 +157,32 @@ private:
     }
 
     void init_Descriptor_Pool() {
-        // 可以参考 blender 中是如何分配的，blender 中有具体的预分配 类型 与 数值
-        std::array<VkDescriptorPoolSize, 3> poolSizes{};
-        poolSizes[0].type            = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-        poolSizes[0].descriptorCount = 1000;
-        poolSizes[1].type            = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-        poolSizes[1].descriptorCount = 1000;
-        poolSizes[2].type            = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-        poolSizes[2].descriptorCount = 1000;
+        static constexpr uint32_t POOL_SIZE_DESCRIPTOR_SETS = 250;
 
+        static constexpr uint32_t POOL_SIZE_STORAGE_BUFFER         = 1000;
+        static constexpr uint32_t POOL_SIZE_STORAGE_IMAGE          = 250;
+        static constexpr uint32_t POOL_SIZE_COMBINED_IMAGE_SAMPLER = 250;
+        static constexpr uint32_t POOL_SIZE_UNIFORM_BUFFER         = 500;
+        static constexpr uint32_t POOL_SIZE_UNIFORM_TEXEL_BUFFER   = 100;
+        static constexpr uint32_t POOL_SIZE_INPUT_ATTACHMENT       = 100;
+
+        std::vector<VkDescriptorPoolSize> pool_sizes = {
+            {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, POOL_SIZE_STORAGE_BUFFER},
+            {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, POOL_SIZE_STORAGE_IMAGE},
+            {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, POOL_SIZE_COMBINED_IMAGE_SAMPLER},
+            {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, POOL_SIZE_UNIFORM_BUFFER},
+            {VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, POOL_SIZE_UNIFORM_TEXEL_BUFFER},
+            {VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, POOL_SIZE_INPUT_ATTACHMENT}
+        };
+
+
+        // 可以参考 blender 中是如何分配的，blender 中有具体的预分配 类型 与 数值
         VkDescriptorPoolCreateInfo descPoolCI{
             .sType         = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO,
             .flags         = VK_DESCRIPTOR_POOL_CREATE_UPDATE_AFTER_BIND_BIT,
-            .maxSets       = 1000,
-            .poolSizeCount = poolSizes.size(),
-            .pPoolSizes    = poolSizes.data(),
+            .maxSets       = POOL_SIZE_DESCRIPTOR_SETS,
+            .poolSizeCount = static_cast<uint32_t>(pool_sizes.size()),
+            .pPoolSizes    = pool_sizes.data(),
         };
         VK_CHECK_RESULT(vkCreateDescriptorPool(get_device(), &descPoolCI, nullptr, &descriptorPool));
     }
