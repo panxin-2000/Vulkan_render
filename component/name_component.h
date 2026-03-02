3523c8ad93e51aca7a1c708394d10e2515c8e734
panxin-2000 <2291162503@qq.com>
2026-03-02 22:10:07 +0800

    添加了 get_entity_name 函数

--- component/name_component.h
+++ component/name_component.h
@@ -10,4 +10,12 @@ struct Name_component {
     std::string name;
 };
 
+inline std::string get_entity_name(const entt::entity entity) {
+    if (const auto name = g_entt().try_get<Name_component>(entity)) {
+        return name->name;
+    }
+    return "";
+}
+
+
 #endif //HELLO_MAC_ENTITY_NAME_COMPONENT_H
