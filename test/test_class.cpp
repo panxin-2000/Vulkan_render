1d1d079360f6d3d22e3a1dca251853f6bb3c68de
panxin-2000 <2291162503@qq.com>
2026-03-06 19:16:22 +0800

    C++ 关于 析构函数的 三五原则

--- test/test_class.cpp
+++ test/test_class.cpp
@@ -61,11 +61,93 @@ TEST(class_access2, class_access) {
     // 一直没有在意过指针对内存的影响
 
     c.data.number_a = 20;
-    c.Left_child = nullptr;
-    c.Right_child = nullptr;
+    c.Left_child    = nullptr;
+    c.Right_child   = nullptr;
     // c.Father_Node = nullptr;
     ASSERT_EQ(c.data.number_a, 20);
     // 想要可以展开一个结构体，那么可以使用class
     // 其实可以这样理解，不管你是偏移一次还是偏移两次，
     // 真实的到汇编的时候应该都是计算一个偏移
+}
+
+class random_func {
+public:
+    int value;
+    std::string name_;
+
+    random_func(int a, std::string name) {
+        value = a;
+        name_ = name;
+        std::cout << name_ << " 构造函数 " << value << std::endl;
+    }
+
+    ~random_func() {
+        if (value == 0) {
+            std::cout << name_ << " 析构函数 但是不需要释放资源 " << value << std::endl;
+        } else {
+            std::cout << name_ << " 析构函数 " << value << std::endl;
+        }
+    };
+
+    // 复制构造函数 和 复制赋值运算符 需要执行 两遍 析构函数
+
+    // 移动构造函数 和 移动赋值运算符 只会执行 一遍 析构函数
+    // 被移动的内容会执行一个 清理 ，不清理
+
+    random_func(const random_func &other)
+        : value(other.value) {
+        std::cout << name_ << " 复制构造函数" << other.name_ << value << std::endl;
+    }
+
+    random_func(random_func &&other) noexcept
+        : value(other.value) {
+        std::cout << other.name_ << " 需要清理的句柄或其他 " << value << std::endl;
+        other.value = 0;
+        std::cout << name_ << " 移动构造函数 清理完成后的值" << other.name_ << value << std::endl;
+    }
+
+    random_func &operator=(const random_func &other) {
+        if (this == &other)
+            return *this;
+        value = other.value;
+        std::cout << name_ << " 复制赋值运算符" << other.name_ << value << std::endl;
+        return *this;
+    }
+
+    random_func &operator=(random_func &&other) noexcept {
+        if (this == &other)
+            return *this;
+        value = other.value;
+        std::cout << other.name_ << " 需要清理的句柄或其他 " << value << std::endl;
+        other.value = 0;
+        std::cout << name_ << " 移动赋值运算符 清理完成后的值 " << other.name_ << value << std::endl;
+
+        return *this;
+    }
+};
+
+TEST(move_and_copy, construction_and_destruction) {
+    random_func a = {10, "a"};
+    random_func b = {20, "b"};
+
+    random_func e = {10, "e"};
+    random_func f = {20, "f"};
+    EXPECT_EQ(a.value, 10);
+    EXPECT_EQ(b.value, 20);
+    b = (a);
+    EXPECT_EQ(a.value, 10);
+    EXPECT_EQ(b.value, 10);
+
+    b = std::move(a);
+
+    EXPECT_EQ(a.value, 0);
+    EXPECT_EQ(b.value, 10);
+
+    random_func c{e};
+    EXPECT_EQ(c.value, 10);
+    EXPECT_EQ(e.value, 10);
+
+    random_func d{std::move(f)};
+    EXPECT_EQ(d.value, 20);
+    EXPECT_EQ(f.value, 0);
 }
