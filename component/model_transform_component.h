c190d70c82f53ba468b74ba9b7d5ec2b1cd1a2f3
panxin-2000 <2291162503@qq.com>
2026-03-18 21:08:01 +0800

    get_screen_ray 移动函数位置

--- component/model_transform_component.h
+++ component/model_transform_component.h
@@ -233,4 +233,54 @@ static entt::entity &get_world_root() {
 }
 
 
+inline Ray<Point_3> &get_screen_ray(const Point_2 mouse_positon) {
+    static Point_2 last_mouse_position = {0, 0};
+    static Ray<Point_3> last_ray       = {{0, 0, 0}, {0, 0, 1}};
+    if (mouse_positon == last_mouse_position) {
+        return last_ray;
+    }
+    last_mouse_position = mouse_positon;
+    // 上面是一个简短记忆上一次的代码
+
+    auto world_entity     = get_world_root();
+    auto camera           = Logic_entt().try_get<camera_optical_component>(world_entity);
+    const auto camera_pos = Logic_entt().try_get<model_transform>(world_entity);
+
+    const auto &backend    = VK_backend::get();
+    auto [width, height]   = backend.get_current_extent();
+    const auto projection  = camera->get_projection();
+    const auto view_matrix = camera_pos->get_view_projection();
+
+    // 1. 转换到 NDC 坐标 (假设鼠标坐标为 mouseX, mouseY)
+    // 这里有一个坑，gltf 给出的坐标和拿到的 显示区域的宽和高差两倍
+    float x = (4.0f * mouse_positon.x) / width - 1.0f;
+    float y = (4.0f * mouse_positon.y) / height - 1.0f; // 注意：Vulkan/GLFW 的 Y 轴通常需要反转
+
+    // 2. 构造近裁剪面和远裁剪面的点 (在裁剪空间)
+    // Vulkan 的近平面通常是 z=0.0，远平面是 z=1.0
+    Eigen::Vector4f ray_start_clip(x, y, 0.0f, 1.0f);
+    Eigen::Vector4f ray_end_clip(x, y, 1.0f, 1.0f);
+
+    // 3. 计算逆矩阵
+    Eigen::Matrix4f invVP = (projection * view_matrix).inverse();
+
+    // 4. 转换回世界空间
+    Eigen::Vector4f world_start = invVP * ray_start_clip; // 这里给出来的是近平面上的起始点
+    Eigen::Vector4f world_end   = invVP * ray_end_clip;
+
+    // 5. 透视除法 (W 分量归一化)
+    world_start /= world_start.w();
+    world_end   /= world_end.w();
+    auto offset = camera_pos->get_offset(); // 这里给出的相机的位置
+    // 6. 确定射线
+    Eigen::Vector3f ray_origin    = world_start.head<3>();
+    Eigen::Vector3f ray_direction = (world_end.head<3>() - ray_origin).normalized();
+    last_ray                      = {
+        {offset},
+        {ray_direction.x(), ray_direction.y(), ray_direction.z()}
+    };
+    return last_ray;
+}
+
+
 #endif //HELLO_MAC_RENDER_COMPONENT_H
--- input_device/device_input_event_deal.cpp
+++ input_device/device_input_event_deal.cpp
@@ -221,46 +221,6 @@ static wmOperatorStatus world_root_on_Event(const entt::entity entity, const bas
 }
 
 
-Ray<Point_3> get_screen_ray(const base_event_with_stamp &event) {
-    auto world_entity     = get_world_root();
-    auto camera           = Logic_entt().try_get<camera_optical_component>(world_entity);
-    const auto camera_pos = Logic_entt().try_get<model_transform>(world_entity);
-
-    const auto &backend    = VK_backend::get();
-    auto [width, height]   = backend.get_current_extent();
-    const auto projection  = camera->get_projection();
-    const auto view_matrix = camera_pos->get_view_projection();
-
-    // 1. 转换到 NDC 坐标 (假设鼠标坐标为 mouseX, mouseY)
-    // 这里有一个坑，gltf 给出的坐标和拿到的 显示区域的宽和高差两倍
-    float x = (4.0f * event.current_position.x) / width - 1.0f;
-    float y = (4.0f * event.current_position.y) / height - 1.0f; // 注意：Vulkan/GLFW 的 Y 轴通常需要反转
-
-    // 2. 构造近裁剪面和远裁剪面的点 (在裁剪空间)
-    // Vulkan 的近平面通常是 z=0.0，远平面是 z=1.0
-    Eigen::Vector4f ray_start_clip(x, y, 0.0f, 1.0f);
-    Eigen::Vector4f ray_end_clip(x, y, 1.0f, 1.0f);
-
-    // 3. 计算逆矩阵
-    Eigen::Matrix4f invVP = (projection * view_matrix).inverse();
-
-    // 4. 转换回世界空间
-    Eigen::Vector4f world_start = invVP * ray_start_clip; // 这里给出来的是近平面上的起始点
-    Eigen::Vector4f world_end   = invVP * ray_end_clip;
-
-    // 5. 透视除法 (W 分量归一化)
-    world_start /= world_start.w();
-    world_end   /= world_end.w();
-    auto offset = camera_pos->get_offset(); // 这里给出的相机的位置
-    // 6. 确定射线
-    Eigen::Vector3f ray_origin    = world_start.head<3>();
-    Eigen::Vector3f ray_direction = (world_end.head<3>() - ray_origin).normalized();
-    Ray<Point_3> result           = {
-        {offset},
-        {ray_direction.x(), ray_direction.y(), ray_direction.z()}
-    };
-    return result;
-}
 
 #include "base_geometry/intersect_function.h"
 
@@ -312,7 +272,7 @@ void base_event_dealing(const base_event_with_stamp &event) {
 
     // 找到当前区域的一个递归栈
 
-    auto ray = get_screen_ray(event);
+    auto ray = get_screen_ray(event.current_position);
     LOG_INFO(g_log(), "ray {}  {}  {}   direction {} {} {}  ", ray.point.x, ray.point.y, ray.point.z,
              ray.direction.x, ray.direction.y, ray.direction.z);
 
