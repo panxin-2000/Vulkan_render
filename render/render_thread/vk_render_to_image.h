686fa3513ad9098e8d1715c96f072ac6127a4d9d
panxin-2000 <2291162503@qq.com>
2026-04-02 01:20:33 +0800

    简单的添加了一些注释

--- render/render_thread/vk_render_to_image.h
+++ render/render_thread/vk_render_to_image.h
@@ -28,6 +28,33 @@
 #include "sets_and_bindings_layout.h"
 
 
+struct float4 {
+    float x, y, z, w;
+};
+
+struct FrustumCorners {
+    float4 corners[8];
+};
+
+struct FrustumPlanes {
+    /* [0] left
+     * [1] right
+     * [2] bottom
+     * [3] top
+     * [4] near
+     * [5] far */
+    float4 planes[6];
+};
+
+struct ViewCullingData {
+    /** \note float3 array padded to float4. */
+    /** Frustum corners. */
+    FrustumCorners frustum_corners;
+    FrustumPlanes frustum_planes;
+    float4 bound_sphere;
+};
+
+
 class vk_render_GPU {
     mutable std::mutex mtx;
     std::atomic<bool> have_object_need_update = false;
@@ -60,6 +87,19 @@ public:
                 begin_shadow_pass(handle, time_line);
 
                 // 中间需要添加 被光 照 到的物体，能产生阴影的物体
+                // 这里的时候发生了一点改变，为什么呢？ 单个 mesh 需要多个不同的 render pass
+                // 这也意味着 需要 它 开始需要多个 shader 了
+                // 然后的问题就是，多个 shader 中，哪些是共同的部分，哪些不是？
+                // 这里开始需要什么了呢？ 光，灯光是需要的部分 ，物体在世界空间的位姿也是需要的部分，
+                // 不同的是输出的内容，以及输出的内容在什么时候再次被需要
+                // model_matrix 是 在多个 shader 都保持不变的值。
+
+                // 拿到z值，并反推出在世界空间中的位置需要 view 和 projection 的逆矩阵
+                // 每个灯光，都需要其 view 和 projection 矩阵
+                // 这个时候，其实应该是可以考虑关于遮挡部分的内容了，比如，
+                // 计算平头截体，然后将不在其中的物体给排除出去。
+                // 这个时候需要什么呢？ 物体的包围盒，model ,之后 再与 平头截体进行相交的判断
+                // 之后再是什么呢？ 看看如何将这部分的计算放到GPU中计算
 
                 end_rendering(handle);
                 shadow_pass_barrier(handle, time_line);
