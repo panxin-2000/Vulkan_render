c97091eb23ebb0ea809d53bfdbaf6e51eccf06f5
panxin-2000 <2291162503@qq.com>
2026-07-29 15:55:57 +0800

    需要的 视锥体剔除 函数编辑完成了,剩下的就是组织代码了

--- render/shader/global_shader_common.glsl
+++ render/shader/global_shader_common.glsl
@@ -1,7 +1,12 @@
 //
 // Created by 潘鑫 on 2026/3/13.
 //
-// 更新最缓慢
+
+
+#extension GL_GOOGLE_include_directive: enable
+#include "commom_function_and_struct.glsl"
+
+
 layout (set = 0, binding = 0) uniform sampler2D bindless_samplerColorMap[];
 
 layout (set = 1, binding = 0) uniform global_parameters
@@ -18,36 +23,6 @@ layout (set = 1, binding = 0) uniform global_parameters
 };
 
 
-// 需要 与 PBR_component 布局相同
-struct ShaderMaterial {
-    vec4 baseColorFactor;
-    vec4 emissiveFactor;
-
-    float metallicFactor;
-    float roughnessFactor;
-    float occlusionStrength;
-    float alphaCutoff;
-
-    uint doubleSided; // 是否开始背面剪裁， 叶子、旗帜、纸张等超薄物体 需要为 true
-    uint alphaMode;           // 有三个值
-    //                              OPAQUE (不透明 - 默认)
-    //                              MASK  基于 alphaCutoff 阈值进行“全有或全无”的硬切
-    //                              BLEND (混合/半透明)
-    // 需要一个为全为一的贴图，也就是纯白的贴图
-    uint baseColorTexture; // 基础颜色 贴图
-    uint normalTexture; //
-
-    uint emissiveTexture; // 自发光 贴图
-    uint ORM_Texture; // Occlusion, Roughness, Metallic
-
-    uint pad_1;
-    uint pad_2;
-    // 下面这两个有什么用？
-    //    vec4 diffuseFactor;
-    //    vec4 specularFactor;
-    // 那么总共的字节数 是 16 + 16 + 16 + 16 + 16  = 80 字节
-};
-
 layout (set = 1, binding = 1) readonly buffer global_PBR_parameters {
     ShaderMaterial material[];
 };
@@ -57,159 +32,15 @@ layout (set = 1, binding = 2) readonly buffer global_PBR_indices {
     uint pbr_index[];
 };
 
-struct AABB_box {
-    vec4 centroid_points;
-    vec4 direction_intervals;
-};
-
-layout (set = 1, binding = 3) readonly buffer global_AABB_box {
-    AABB_box aabb_box[];
-};
-
-
-bool IsAABBInFrustum(vec4 frustum_planes[6], AABB_box box)
-{
-    for (int i = 0; i < 6; ++i)
-    {
-        float projectedRadius = dot(box.direction_intervals, abs(frustum_planes[i]));
-        float distanceToCenter = dot(box.centroid_points, frustum_planes[i]);
-        if (distanceToCenter < -projectedRadius)
-        {
-            return false;
-        }
-    }
-    return true;
-}
 
 
 
-vec2 octEncode(vec3 n) {
-    // 1. L1 归一化：确保 |x| + |y| + |z| = 1
-    float l1norm = abs(n.x) + abs(n.y) + abs(n.z);
-    vec2 res = n.xy / l1norm;
-    // normalize 是为了把向量投影到“球体”上，而 L1 归一化是为了把向量投影到“八面体”上。
-    // 坐标绝对值之和等于 1 ，能被平整地展开成 2D 正方形的物理基础
-
-    // 2. 处理下半球 (z < 0) 的翻折
-    // 使用三元表达式或 sign 模拟。在现代编译器中，这种写法通常会被优化为 CMOV 或 BFI 指令
-    vec2 signNotZero = vec2(n.x >= 0.0 ? 1.0 : -1.0, n.y >= 0.0 ? 1.0 : -1.0);
-    // 只是为了确定在那个象限，下层的时候 (1.0 - abs(res.yx)) 计算后得到的都是 正数 ，需要修改象限
-    return (n.z >= 0.0) ? res : (1.0 - abs(res.yx)) * signNotZero;
-    // 折叠后的边界必须与折叠前的边界重合  (1.0 - abs(res.yx)) // 其实重点是上下两层 边界靠近，插值时不会出问题
-
-    // 展开的过程是什么呢？ 想象一个正方形，四个中点组成的正方形 组成了正八面体的 上半部分
-    // 剩余的四个三角形组成了正八面体的下半部分
-    // 四个中点组成的正方形 其 x+y 永远小于1
-    // 剩余的四个三角形，其 |x| + |y| 用于大于1
-    // 举一个例子，x = 0.5 时，y = 0.5  z = 0
-    // 举一个例子，x = 0.25 时，y = 0.25  z = -0.5
-    // 那么需要放置的位置时  x = 0.75 时，y = 0.75
-    // 举一个例子，x = -0.25 时，y = 0.25  z = -0.5
-    // 那么需要放置的位置时  x = -0.75 时，y = 0.75
-
-    // 还需要之后写一个 C 语言版本的，最好能使用 矢量加速器
-
-}
-
-vec3 octDecode(vec2 v) {
-    // 1. 根据 x, y 推导初步的 z
-    vec3 n = vec3(v, 1.0 - abs(v.x) - abs(v.y));
-
-    // 2. 如果 z < 0，利用原 xy 的正负号进行翻折还原
-    // 这行逻辑实现了：当 n.z < 0 时，将 xy 翻折回球面
-    vec2 s = vec2(n.x >= 0.0 ? 1.0 : -1.0, n.y >= 0.0 ? 1.0 : -1.0);
-    n.xy += (n.z < 0.0) ? (abs(n.yx) - 1.0) * s : vec2(0.0);
-
-    return normalize(n);
-}
 
 
-mat4 calculate_matrix(vec3 instancePos, vec3 instanceDir) {
-    vec3 forward = normalize(instanceDir);
-
-    // 2. 定义世界坐标系的临时“上”方向
-    vec3 worldUp = vec3(0.0, 1.0, 0.0);
-    // 防止物体正向上导致叉乘为 0，做一个微小的兜底
-    if (abs(dot(forward, worldUp)) > 0.99) {
-        worldUp = vec3(0.0, 0.0, 1.0);
-    }
-
-    // 3. 叉乘构建互相正交的 X 轴 (right) 和 Y 轴 (up)
-    vec3 right = normalize(cross(worldUp, forward));
-    vec3 up = cross(forward, right);
-
-    // 4. 在 GLSL 中实时动态构建 4x4 变换矩阵
-    // 注意：GLSL 的 mat4 是 列主序 (Column-Major)，传参按列排列
-    mat4 model = mat4(
-    vec4(right, 0.0), // 第一列：X 轴 (旋转)
-    vec4(up, 0.0), // 第二列：Y 轴 (旋转)
-    vec4(forward, 0.0), // 第三列：Z 轴 (旋转)
-    vec4(instancePos, 1.0) // 第四列：位移 (Position)
-    );
-    return model;
-}
-
-// 用正八面体做环境贴图
-// vec3 R = reflect(-V, N);
-// vec2 uv = octEncode(R) * 0.5 + 0.5; // 映射到 [0, 1] 范围
-// // lod 对应粗糙度级别
-// vec3 envColor = textureLod(u_OctahedralEnvMap, uv, lod).rgb;
-
-// 关键挑战：处理边界接缝 (Seams)
-// 在预过滤环境贴图时，八面体的边界是最大的挑战。
-// 问题：硬件的线性过滤（Linear Filtering）在八面体 UV 边界处会采样到错误的像素（因为 UV 空间在这些地方是断开的）。
-// 解决方案：
-// Padding（填充）：在生成 2D 八面体贴图时，在每个边缘外扩展 1-2 个像素，并根据翻折逻辑将对应的颜色填进去。
-// 坐标修正：在 Shader 采样前，对 UV 进行极其微小的缩放，使其避开最外层的像素边缘。
 
 
-struct Light {
-    vec4 pos;
-    vec4 rotate;
-    vec4 color;
-    float intensity;
-    float range;
-    float angle_scale;
-    float angle_offset;
-};
 
 
-vec3 Directional(Light light, vec3 world_pos) {
-    vec3 pos_err = light.pos.xyz - world_pos;
-    float distanceSq = dot(pos_err, pos_err);
-    return light.color.rgb * light.intensity / distanceSq;
-}
 
-vec3 Spot_light(Light light, vec3 world_pos) {
-    vec3 pos_err = light.pos.xyz - world_pos;
-    float distanceSq = dot(pos_err, pos_err);
-    float rangeSq = light.range * light.range;
-    float factor = distanceSq / rangeSq;
-    float smoothFactor = clamp(1.0f - factor * factor, 0.0f, 1.0f);
-    float result = (smoothFactor * smoothFactor) / max(distanceSq, 0.0001f);
-    return light.color.rgb * light.intensity * result;
 
-}
-
-// 在 GLSL 中使用四元数旋转默认的 -Z 轴向量
-vec3 quaternion_transform(vec4 q, vec3 v) {
-    return v + 2.0 * cross(q.xyz, cross(q.xyz, v) + q.w * v);
-}
-
-// NdotL 可以替换 为其他的吗？
-//  float cd 灯光夹角余弦
-vec3 Point_light(Light light, vec3 world_pos, out vec3 L) {
-    vec3 pos_err = world_pos - light.pos.xyz;
-    L = -normalize(pos_err);
-    float distanceSq = dot(pos_err, pos_err);
-    vec3 defaultDir = vec3(0.0, 0.0, -1.0);
-    vec3 light_direction = normalize(quaternion_transform(light.rotate, defaultDir));
-    float cd = dot(light_direction, -L);
-    float rangeSq = light.range * light.range;
-    float factor = distanceSq / rangeSq;
-    float smoothFactor = clamp(1.0f - factor * factor, 0.0f, 1.0f);
-    float result = (smoothFactor * smoothFactor) / max(distanceSq, 0.0001f);
-    float attenuation = clamp(cd * light.angle_scale + light.angle_offset, 0.0, 1.0);
-    return light.color.rgb * light.intensity * result * attenuation;
-}
 
