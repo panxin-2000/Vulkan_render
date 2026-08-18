554ac1c33a3047a9afda9e6de9567d57e4e53843
panxin-2000 <2291162503@qq.com>
2026-08-18 17:42:03 +0800

    消除l重复的函数

--- render/vulkan_code/vulkan_image_view.h
+++ render/vulkan_code/vulkan_image_view.h
@@ -6,17 +6,6 @@
 #define HELLO_MAC_VULKAN_IMAGE_VIEW_H
 
 
-#include <volk.h>
 
-VkImageView createImageView(const VkImage &image,
-                            const VkFormat &format,
-                            const VkImageAspectFlags &aspectFlags,
-                            const uint32_t &mipLevels);
-
-
-VkImageView create_sky_cube_ImageView(const VkImage &image,
-                                      const VkFormat &format,
-                                      const VkImageAspectFlags &aspectFlags,
-                                      const uint32_t &mipLevels);
 
 #endif //HELLO_MAC_VULKAN_IMAGE_VIEW_H
