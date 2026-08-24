ddd0db9d08a9ccae2f07e9cf16433cc58421861f
panxin-2000 <2291162503@qq.com>
2026-08-25 00:26:00 +0800

    消除了 vkCmdBeginRendering 中图片大小不相等的警告

--- render/render_image_manager.cpp
+++ render/render_image_manager.cpp
@@ -15,7 +15,7 @@ void Render_image_manager::using_to_free() {
 }
 
 VKR_image_ptr Render_image_manager::get_one_position_image() {
-    const VkExtent2D extent = VK_backend::instance().get_swap_rational_extent();
+    const VkExtent2D extent = VK_backend::instance().get_current_extent();
     Image_and_view_parameters parameters{
         .format = VK_FORMAT_R16G16B16A16_SFLOAT,
         .width  = extent.width,
@@ -33,7 +33,7 @@ VKR_image_ptr Render_image_manager::get_one_position_image() {
 }
 
 VKR_image_ptr Render_image_manager::get_one_normal_image() {
-    const VkExtent2D extent = VK_backend::instance().get_swap_rational_extent();
+    const VkExtent2D extent = VK_backend::instance().get_current_extent();
     Image_and_view_parameters parameters{
         .format = VK_FORMAT_R16G16B16A16_SFLOAT,
         .width  = extent.width,
@@ -52,7 +52,7 @@ VKR_image_ptr Render_image_manager::get_one_normal_image() {
 }
 
 VKR_image_ptr Render_image_manager::get_one_color_image() {
-    const VkExtent2D extent = VK_backend::instance().get_swap_rational_extent();
+    const VkExtent2D extent = VK_backend::instance().get_current_extent();
     Image_and_view_parameters parameters{
         .format = VK_FORMAT_B8G8R8A8_SRGB,
         .width  = extent.width,
@@ -71,7 +71,7 @@ VKR_image_ptr Render_image_manager::get_one_color_image() {
 }
 
 VKR_image_ptr Render_image_manager::get_one_entity_image() {
-    const VkExtent2D extent = VK_backend::instance().get_swap_rational_extent();
+    const VkExtent2D extent = VK_backend::instance().get_current_extent();
     Image_and_view_parameters parameters{
         .format = VK_FORMAT_R32_UINT,
         .width  = extent.width,
@@ -95,17 +95,19 @@ void Render_image_manager::create() {
 
 VKR_image_ptr Render_image_manager::get_one_depth_image() {
     Image_and_view_parameters parameters{};
-    const VkExtent2D extent = VK_backend::instance().get_swap_rational_extent();
-    parameters.format       = VK_backend::instance().get_depth_format();
-    parameters.width        = extent.width;
-    parameters.height       = extent.height;
-    parameters.depth        = 1;
-    parameters.usage        = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
-    parameters.aspectMask   = VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
-    parameters.tiling       = VK_IMAGE_TILING_OPTIMAL;
-    parameters.mipLevels    = 1;
-    parameters.arrayLayers  = 1;
-    parameters.flags        = 0;
+    // 唯一可能的问题就是在这里了,
+    const VkExtent2D extent = VK_backend::instance().get_current_extent();
+    //
+    parameters.format      = VK_backend::instance().get_depth_format();
+    parameters.width       = extent.width;
+    parameters.height      = extent.height;
+    parameters.depth       = 1;
+    parameters.usage       = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+    parameters.aspectMask  = VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT;
+    parameters.tiling      = VK_IMAGE_TILING_OPTIMAL;
+    parameters.mipLevels   = 1;
+    parameters.arrayLayers = 1;
+    parameters.flags       = 0;
 
 
     return find(parameters);
@@ -113,11 +115,12 @@ VKR_image_ptr Render_image_manager::get_one_depth_image() {
 
 VKR_image_ptr Render_image_manager::get_one_depth_AO_image() {
     Image_and_view_parameters parameters{};
-    parameters.format = VK_FORMAT_D32_SFLOAT;
-    parameters.width  = 2016;
-    parameters.height = 1832;
-    parameters.depth  = 1;
-    parameters.usage  = static_cast<VkImageUsageFlagBits>(
+    const VkExtent2D extent = VK_backend::instance().get_current_extent();
+    parameters.format       = VK_FORMAT_D32_SFLOAT;
+    parameters.width        = extent.width;
+    parameters.height       = extent.height;
+    parameters.depth        = 1;
+    parameters.usage        = static_cast<VkImageUsageFlagBits>(
         VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT);
     parameters.aspectMask  = VK_IMAGE_ASPECT_DEPTH_BIT;
     parameters.tiling      = VK_IMAGE_TILING_OPTIMAL;
@@ -146,10 +149,11 @@ VKR_image_ptr Render_image_manager::get_one_shadow_image() {
 }
 
 VKR_image_ptr Render_image_manager::get_one_depth_SSAO_image() {
+    const VkExtent2D extent = VK_backend::instance().get_current_extent();
     Image_and_view_parameters parameters{};
     parameters.format = VK_FORMAT_R8_UNORM;
-    parameters.width  = 2016;
-    parameters.height = 1832;
+    parameters.width  = extent.width;
+    parameters.height = extent.height;
     parameters.depth  = 1;
     parameters.usage  = static_cast<VkImageUsageFlagBits>(
         VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT);
