9c977c2b0198a2219029bac442a07d5236cda81a
panxin-2000 <2291162503@qq.com>
2026-08-26 14:51:45 +0800

    设置一个不错的太阳的位置

--- main.cpp
+++ main.cpp
@@ -135,7 +135,7 @@ int main(int argc, char *argv[]) {
     bool show_another_window = false;
     ImVec4 clear_color       = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     auto imgui_entity        = create_imgui_entity("imgui", nullptr);
-    Point_3 world_light_pos(0.0f, 1.0f, 0.0f);
+    Point_3 world_light_pos(0.250f, -1.0f, 0.750f);
 
     // Render loop
     float run_time = 0;
