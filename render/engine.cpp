814a0c263440bd1cc2767453a151e742f5674ad0
panxin-2000 <2291162503@qq.com>
2026-09-03 12:10:55 +0800

    以需要显示的像素数量的1/2进行渲染

--- render/engine.cpp
+++ render/engine.cpp
@@ -395,7 +395,9 @@ void Engine::update_global_parameter(std::optional<Texture_parameter> offscreen,
     global_descriptor_sets_index = (global_descriptor_sets_index + 1) % 3;
     std::map<std::string, Update_descriptor_binding> update_global_descriptor_sets;
     const auto extent              = VK_backend::instance().get_current_extent();
-    global_parameters_.screen_size = {static_cast<float>(extent.width), static_cast<float>(extent.height), 0, 0};
+    global_parameters_.screen_size = {
+        static_cast<float>(extent.width / 2), static_cast<float>(extent.height / 2), 0, 0
+    };
 
     global_parameters_.frustum_planes =
             get_Frustum_Planes(global_parameters_.projection_matrix * global_parameters_.view_matrix);
