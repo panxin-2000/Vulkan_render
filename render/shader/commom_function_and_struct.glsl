ad78be8fec7b1b1b774d48038a20ac105a206df7
panxin-2000 <2291162503@qq.com>
2026-09-06 01:09:42 +0800

    简单调换一下矩阵的顺序

--- render/shader/commom_function_and_struct.glsl
+++ render/shader/commom_function_and_struct.glsl
@@ -10,6 +10,8 @@ struct VkDrawIndexedIndirectCommand {
 };
 const float PI = 3.14159265359;
 
+#define EIGEN_INDEX(m, row, col) (m)[col][row]
+
 
 struct Frustum {
     vec4 frustum_planes[6];
@@ -975,8 +977,8 @@ vec3 get_view_pos(vec2 uv, float depth, mat4 invProjection){
     vec4 clipPos = vec4(uv * 2.0 - 1.0, depth, 1.0);
     float x = invProjection[0][0] * clipPos.x;
     float y = invProjection[1][1] * clipPos.y;
-    float z = invProjection[2][2] * clipPos.z + invProjection[3][2];
-    float w = invProjection[2][3] * clipPos.z + invProjection[3][3];
+    float z = EIGEN_INDEX(invProjection,2,2) * clipPos.z + EIGEN_INDEX(invProjection,2,3);
+    float w = EIGEN_INDEX(invProjection,3,2) * clipPos.z + EIGEN_INDEX(invProjection,3,3);
     return vec3(x, y, z) / w;
 }
 
@@ -1166,8 +1168,8 @@ float get_noise_v2(uvec2 p, uint time_seed) {
  */
 float linearize_depth_vulkan_universal(float depth, mat4 Projection) {
     // 直接提取矩阵中掌管 NDC 到 ViewSpace 深度缩放与偏移的两大核心系数
-    float P22 = Projection[2][2];
-    float P32 = Projection[2][3];
+    float P22 = EIGEN_INDEX(Projection, 2,2);
+    float P32 = EIGEN_INDEX(Projection, 3,2);
     // 不管是常规 Z 还是 Reversed-Z，甚至包括“无限远景 Reversed-Z”，
     // 它们在代数化简后，在 GPU 硬件层面全都能完美收敛到这个极简的分式中！
     return P32 / (depth - P22);
